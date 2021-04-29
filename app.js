e5f669cea5a6a885a15ea6b1f81ce51fd5a25df5
Daniel <elrion018@gmail.com>
2021-04-29 22:26:29 +0900

    feature : add cors setting

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+
+app.all('/*', function (request, response, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+
+server.listen(3000, function () {
+  console.log('socket.io');
+});
